fe4a3f8f4b6649e8e07d10dd63cfd4b953df4ed7
gusmaogabriels <gusmaogabriels@gmail.com>
2020-03-29 16:27:31 -0400

    Clean-up 8.1.8

--- espresso/espresso.py
+++ espresso/espresso.py
@@ -962,7 +962,7 @@ class Espresso(FileIOCalculator, object):
             self.logfile = open(self.log, 'ab')
             if self.site.usehostfile:
                 self.site.write_local_hostfile()
-            self.write_input()
+            self.write_input(atoms)
                 
         self.command = self.site.get_proc_mpi_command(self.scratch,
                                 'pw.x ' + self.parflags + ' -in {0}/pw.inp'.format(self.localtmp))
